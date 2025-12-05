e36f9b73ef706615cf9bedd87c526a1d1ac4f33b
Pavel Belov <Pavel.Belov@x-tensive.com>
2025-12-05 15:08:28 +0500

    tests

--- src/test/fakeTTS.py
+++ src/test/fakeTTS.py
@@ -1,10 +1,11 @@
 from iSynthesizer import ISynthesizer
 import os
 
+
 class FakeTTS(ISynthesizer):
     def __init__(self):
-        self.path = os.path.join("data", "test.wav")
+        self.path = os.path.join("test", "test.wav")
 
     def synthesize(self, text: str) -> bytes:
-         with open(self.path, "rb") as f:
+        with open(self.path, "rb") as f:
             return f.read()
--- src/test/wyomingPlayer.py
+++ src/test/wyomingPlayer.py
@@ -2,22 +2,25 @@ import asyncio
 import wave
 import sounddevice as sd
 import soundfile as sf
+import os
 
 from wyoming.tts import Synthesize
 from wyoming.client import AsyncClient
 
 
 class AuddioInfo:
-  def __init__(self, rate, width, channels):
-      self.rate = rate
-      self.width = width
-      self.channels = channels
-      self.payload = []
+    def __init__(self, rate, width, channels):
+        self.rate = rate
+        self.width = width
+        self.channels = channels
+        self.payload = []
+
 
 wyominUri = input("uri: ")
-if wyominUri == '':
+if wyominUri == "":
     wyominUri = "tcp://127.0.0.1:10200"
     print(wyominUri)
+
 
 async def _proxy_to_wyoming(synth: Synthesize) -> None:
     async with AsyncClient.from_uri(wyominUri) as client:
@@ -25,7 +28,9 @@ async def _proxy_to_wyoming(synth: Synthesize) -> None:
         event = await client.read_event()
         if event.type != "audio-start":
             return None
-        result = AuddioInfo(event.data["rate"],event.data["width"], event.data["channels"]) 
+        result = AuddioInfo(
+            event.data["rate"], event.data["width"], event.data["channels"]
+        )
         event = await client.read_event()
         while event.type == "audio-chunk":
             result.payload.append(event.payload)
@@ -37,16 +42,17 @@ async def test_server():
     while True:
         text = Synthesize(input("text: "))
         response = await _proxy_to_wyoming(text)
+        testFileName = os.path.join("test", "result.wav")
         if not response == None:
-            with wave.open('c:\\tmp\\output.wav', 'wb') as wav_file:
+            with wave.open(testFileName, "wb") as wav_file:
                 wav_file.setnchannels(response.channels)
                 wav_file.setsampwidth(response.width)
                 wav_file.setframerate(response.rate)
-                wav_file.writeframes(b''.join(response.payload))
+                wav_file.writeframes(b"".join(response.payload))
 
-            data, samplerate = sf.read('c:\\tmp\\output.wav')
+            data, samplerate = sf.read(testFileName)
             sd.play(data, samplerate)
-            sd.wait() 
+            sd.wait()
 
 
 if __name__ == "__main__":
